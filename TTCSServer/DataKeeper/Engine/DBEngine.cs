157a70e20abcee6a8acc71a369eb46b29375dd51
Champ <13belove@gmail.com>
2017-05-28 23:47:49 +0700

    + add function to check dome opened.

--- TTCSServer/DataKeeper/Engine/DBEngine.cs
+++ TTCSServer/DataKeeper/Engine/DBEngine.cs
@@ -75,27 +75,85 @@ namespace DataKeeper.Engine
             });
         }
 
+        /*
+                        if(FieldName.ToString().Equals("ASTROHEVEN_SHUTTERA_STATUS"))
+                        {
+                            db.shutter_a = true;
+                        }
+                        else if(FieldName.ToString().Equals("ASTROHEVEN_SHUTTERB_STATUS"))
+                        {
+                            db.shutter_b = true;
+                        }
+          
+         */
+
         public void insert_dome_open(String StationName, String DeviceName, String FieldName, String Value, DateTime AccessDate, DateTime DataTimestamp)
         {
             Task TTask = Task.Run(async () =>
             {
-                var collection = _database.GetCollection<BsonDocument>(StationName + "_" + DeviceName + "_" + FieldName + "_STATE");
-                var builder = Builders<BsonDocument>.Filter;
-                var cursor = collection.Find(builder.Eq("State", "OPENED"));
+                var dome_side = "";
 
-                var count = cursor.CountAsync();
-
-                if(count.Result <= 0) //NO OPENED State, Do insert to DB.
+                if(Value.ToString().Equals("ASTROHEVEN_SHUTTERA_STATUS"))
                 {
-                    var document = new BsonDocument
-                    {
-                        { "State", "OPEN" },
-                        { "OpenDate", AccessDate.ToString("yyyy-MM-dd HH:mm:ss") },
-                        { "CloseDate", "" },
-                        { "Updated", DataTimestamp.ToString("yyyy-MM-dd HH:mm:ss") }
-                    };
+                    dome_side = "A";
+                }
+                else if (Value.ToString().Equals("ASTROHEVEN_SHUTTERB_STATUS"))
+                {
+                    dome_side = "B";
+                }
 
-                    await collection.InsertOneAsync(document);
+                if (dome_side != "")
+                {
+                    var collection = _database.GetCollection<BsonDocument>(StationName + "_" + DeviceName + "_TEMP");
+                    var builder = Builders<BsonDocument>.Filter;
+                    var filter = builder.Eq("SHUTTER", dome_side) & builder.Eq("State", "OPEN") & builder.Eq("CloseDate", "");
+                    var cursor = collection.Find(filter);
+
+                    var count = cursor.CountAsync();
+
+                    if (count.Result <= 0) //NO OPENED State, Do insert to DB.
+                    {
+                        var document = new BsonDocument
+                        {
+                            { "SHUTTER", dome_side },
+                            { "State", "OPEN" },
+                            { "OpenDate", AccessDate.ToString("yyyy-MM-dd HH:mm:ss") },
+                            { "CloseDate", "" },
+                            { "Updated", DataTimestamp.ToString("yyyy-MM-dd HH:mm:ss") }
+                        };
+
+                        await collection.InsertOneAsync(document);
+                    }
+
+                    var collection3 = _database.GetCollection<BsonDocument>(StationName + "_" + DeviceName + "_TEMP");
+                    var builder3 = Builders<BsonDocument>.Filter;
+                    var filter3 = builder.Eq("State", "OPEN");
+                    var cursor3 = collection3.Find(filter);
+
+                    var count3 = cursor3.CountAsync();
+
+                    if (count3.Result <= 0)
+                    {
+                        var collection2 = _database.GetCollection<BsonDocument>(StationName + "_" + DeviceName + "_STATE");
+                        var builder2 = Builders<BsonDocument>.Filter;
+                        var filter2 = builder2.Eq("State", "OPEN") & builder.Eq("CloseDate", "");
+                        var cursor2 = collection2.Find(filter);
+
+                        var count2 = cursor2.CountAsync();
+
+                        if (count2.Result <= 0) //NO OPENED State, Do insert to DB.
+                        {
+                            var document = new BsonDocument
+                            {
+                                { "State", "OPEN" },
+                                { "OpenDate", AccessDate.ToString("yyyy-MM-dd HH:mm:ss") },
+                                { "CloseDate", "" },
+                                { "Updated", DataTimestamp.ToString("yyyy-MM-dd HH:mm:ss") }
+                            };
+
+                            await collection.InsertOneAsync(document);
+                        }
+                    }
                 }
             });
         }
@@ -104,12 +162,43 @@ namespace DataKeeper.Engine
         {
             Task TTask = Task.Run(async () =>
             {
-                var collection = _database.GetCollection<BsonDocument>(StationName + "_" + DeviceName + "_" + FieldName + "_STATE");
-                var builder = Builders<BsonDocument>.Filter;
-                var filter = builder.Eq("State", "OPEN") & builder.Eq("CloseDate", "");
-                var update = Builders<BsonDocument>.Update.Set("State", "CLOS").Set("ClosedDate", AccessDate.ToString("yyyy-MM-dd HH:mm:ss")).Set("Updated", DataTimestamp.ToString("yyyy-MM-dd HH:mm:ss"));
+                var dome_side = "";
 
-                await collection.UpdateManyAsync(filter, update);
+                if (Value.ToString().Equals("ASTROHEVEN_SHUTTERA_STATUS"))
+                {
+                    dome_side = "A";
+                }
+                else if (Value.ToString().Equals("ASTROHEVEN_SHUTTERB_STATUS"))
+                {
+                    dome_side = "B";
+                }
+
+                if (dome_side != "")
+                {
+                    var collection = _database.GetCollection<BsonDocument>(StationName + "_" + DeviceName + "_TEMP");
+                    var builder = Builders<BsonDocument>.Filter;
+                    var filter = builder.Eq("SHUTTER", dome_side) & builder.Eq("State", "OPEN") & builder.Eq("CloseDate", "");
+                    var update = Builders<BsonDocument>.Update.Set("State", "CLOSE").Set("ClosedDate", AccessDate.ToString("yyyy-MM-dd HH:mm:ss")).Set("Updated", DataTimestamp.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                    await collection.UpdateManyAsync(filter, update);
+
+                    var collection3 = _database.GetCollection<BsonDocument>(StationName + "_" + DeviceName + "_TEMP");
+                    var builder3 = Builders<BsonDocument>.Filter;
+                    var filter3 = builder.Eq("State", "OPEN");
+                    var cursor3 = collection3.Find(filter);
+
+                    var count3 = cursor3.CountAsync();
+
+                    if (count3.Result <= 0)
+                    { 
+                        var collection2 = _database.GetCollection<BsonDocument>(StationName + "_" + DeviceName + "_STATE");
+                        var builder2 = Builders<BsonDocument>.Filter;
+                        var filter2 = builder.Eq("State", "OPEN") & builder.Eq("CloseDate", "");
+                        var update2 = Builders<BsonDocument>.Update.Set("State", "CLOSE").Set("ClosedDate", AccessDate.ToString("yyyy-MM-dd HH:mm:ss")).Set("Updated", DataTimestamp.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                        await collection.UpdateManyAsync(filter, update);
+                    }
+                }
             });
         }
     }
