da15ef901d9adf17c44c61088e24dfe8a97d664e
pakawat <pakawat_prasit@hotmail.com>
2017-06-01 22:25:08 +0700

    + add SQM Unit to database.

--- TTCSServer/DataKeeper/Engine/DBEngine.cs
+++ TTCSServer/DataKeeper/Engine/DBEngine.cs
@@ -34,6 +34,22 @@ namespace DataKeeper.Engine
             });
         }
 
+        public void insert_unit(String StationName, String DeviceName, String FieldName, String Value, String Unit, DateTime DataTimestamp)
+        {
+            var document = new BsonDocument
+                    {
+                        { "Values", Value },
+                        { "Unit", Unit },
+                        { "Updated", DataTimestamp.ToString("yyyy-MM-dd HH:mm:ss") }
+                    };
+
+            Task TTask = Task.Run(async () =>
+            {
+                var collection = _database.GetCollection<BsonDocument>(StationName + "_" + DeviceName + "_" + FieldName);
+                await collection.InsertOneAsync(document);
+            });
+        }
+
         public void insert_user_login(String StationName, String DeviceName, String FieldName, String Value, String State, DateTime AccessDate, DateTime DataTimestamp)
         {
             Task TTask = Task.Run(async () =>
@@ -41,7 +57,7 @@ namespace DataKeeper.Engine
                 var collection = _database.GetCollection<BsonDocument>(StationName + "_" + DeviceName + "_" + FieldName);
                 var builder = Builders<BsonDocument>.Filter;
                 var filter = builder.Eq("Values", Value) & builder.Eq("State", "LOGIN") & builder.Eq("LogoutDate", "");
-                var update = Builders<BsonDocument>.Update.Set("State", State).Set("LogoutDate", AccessDate.ToString("yyyy-MM-dd HH:mm:ss")).Set("Updated", DataTimestamp.ToString("yyyy-MM-dd HH:mm:ss"));
+                var update = Builders<BsonDocument>.Update.Set("State", "LOGOUT").Set("LogoutDate", AccessDate.ToString("yyyy-MM-dd HH:mm:ss")).Set("Updated", DataTimestamp.ToString("yyyy-MM-dd HH:mm:ss"));
 
                 await collection.UpdateManyAsync(filter, update);
             });
